a3f1170e4a71568908078558eca6c346f1df161d
Radical Egg <luther.jaymen@gmail.com>
2021-06-29 14:54:19 -0400

    moved database location to an environment variable

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 require('dotenv').config();
 const bot = require(__dirname + '/modules/Eggcart.js')
 
-const eggCart = new bot("/Users/jaymenluther/git/eggcart/shopping.db")
+const eggCart = new bot(String(process.env.DATABASE_LOCATION))
+
 eggCart.addItem()
 eggCart.getList()
 eggCart.deleteItem()
 eggCart.clearList()
 eggCart.help()
 eggCart.connect()
-
-
-
