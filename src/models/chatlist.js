4522830d19442ffe13aaba13adf9a26857f511f8
Luis González <ljgonzalez@uc.cl>
2024-01-17 20:06:50 -0300

    ✨ feat(models): Add associations between ChatList and EggoList models.

--- src/models/chatlist.js
+++ src/models/chatlist.js
@@ -10,7 +10,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      ChatList.hasMany(models.EggoList, {
+        foreignKey: 'chatListId',
+        as: 'eggolists'
+      });
     }
   }
   ChatList.init({
